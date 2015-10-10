576a2ecb39507f2fe872e92bd9ca038097203da9
Brandon Huang <hhe.brandon@berkeley.edu>
2015-10-10 16:33:16 -0700

    Add non-depth-searching function to parse.py

--- wiki-parse/parse.py
+++ wiki-parse/parse.py
@@ -1,9 +1,11 @@
 import wikipedia
 
 # Returns list of lists: elements are lines of Wikipedia text delimited by newline
-def query(querystr, depth):
+# Recursively finds new pages via "See also" section
+def rec_parse(querystr, depth):
     text_list = []
-    page = wikipedia.page(querystr)
+    results = wikipedia.search(querystr)
+    page = wikipedia.page(results[0])
 
     def parse(page, depth):
         if (depth > 0 and page.section("See also")):
@@ -14,3 +16,13 @@ def query(querystr, depth):
 
     parse(page, depth)
     return text_list
+
+# Returns list of lists: elements are lines of Wikipedia text delimited by newline (same return behavior)
+# Finds new pages by iterating through the search results
+def iter_parse(querystr):
+    text_list = []
+    results = wikipedia.search(querystr)
+    for page in results:
+        text_list.append(page.content.splitlines())
+    return text_list
+j
