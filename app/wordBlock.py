898d6e4fa474ef732ec331e3bececdc73b3c5b21
Evan Sheng <evansheng112@berkeley.edu>
2015-10-10 23:36:08 -0700

    backend works

--- app/wordBlock.py
+++ app/wordBlock.py
@@ -5,10 +5,12 @@ from thes import get_synonyms
 
 def main(Subject, word):
     retList = []
-    depth = 2
-    data_list = depth_parse(Subject, depth); #get our LARGE dataset from wiki on subject content
+    depth = 1
+    data_list = depth_parse(Subject, depth, 5); #get our LARGE dataset from wiki on subject content
+    #data_list still contains unicode shit that cant be printed!
     synonyms = get_synonyms(word); #get synonyms from thesaurus.com
-    candidates = nltk.ContextIndex(data_list).similar_words(word, n=10000)
+    context = nltk.ContextIndex(data_list)
+    candidates = context.similar_words(word, n=10000)
     for candidate in candidates:
         if candidate in synonyms:
             retList.append(candidate)
