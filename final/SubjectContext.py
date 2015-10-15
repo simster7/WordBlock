b7df597baaf5c5c396641e89eed81ea437fb0e95
Simon Behar <simbeh7@gmail.com>
2015-10-15 11:17:18 -0700

    'removed antonyms'

--- final/SubjectContext.py
+++ final/SubjectContext.py
@@ -71,7 +71,10 @@ class SubjectContext:
         
         html = opener.open('http://www.thesaurus.com/browse/' + word).read(100000).\
                 decode(encoding='utf-8', errors='replace')
-        
+
+        antonym_index = html.index('<section class="container-info antonyms">')
+        html = html[:antonym_index] + html[html.index('</section>', antonym_index)]
+                   
         synonyms_a = re.findall(r'<span class="text">([a-z\ ]+)<\/span>', html)
         synonyms_b = re.findall(r'<a href="[^\"]+">\n([a-z\ ]+)<\/a>', html)
 
